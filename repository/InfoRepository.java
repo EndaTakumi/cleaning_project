11210fce99e443432bbea15c1bf28dd546cbe636
EndaTakumi <takumi_enda@star-system.jp>
2021-12-22 09:34:34 +0900

    update repository

--- repository/InfoRepository.java
+++ repository/InfoRepository.java
@@ -1,9 +1,14 @@
 package jp.co.sss.project.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import jp.co.sss.project.entity.Info;
 
 public interface InfoRepository extends JpaRepository<Info, Integer>{
 	
+	@Query("SELECT i FROM Info i WHERE current_date BETWEEN keisaiFrom AND keisaiTo")
+	List<Info> findByKeisaiDateBetween();
 }
