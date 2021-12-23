bdeed4285f191d73c051af75f5a42b3234ad174e
EndaTakumi <takumi_enda@star-system.jp>
2021-12-23 10:33:18 +0900

    update controller

--- controller/IndexController.java
+++ controller/IndexController.java
@@ -1,6 +1,7 @@
 package jp.co.sss.project.controller;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
@@ -15,13 +16,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import jp.co.sss.project.entity.Customer;
 import jp.co.sss.project.entity.Goods;
-import jp.co.sss.project.entity.Tax;
+import jp.co.sss.project.entity.Hist;
+import jp.co.sss.project.entity.Histdetail;
 import jp.co.sss.project.form.LoginFormValidation;
 import jp.co.sss.project.form.ProductNumberList;
 import jp.co.sss.project.repository.CustomerRepository;
 import jp.co.sss.project.repository.EmployeeRepository;
 import jp.co.sss.project.repository.GoodsRepository;
+import jp.co.sss.project.repository.HistRepository;
+import jp.co.sss.project.repository.HistdetailRepository;
 import jp.co.sss.project.repository.InfoRepository;
 import jp.co.sss.project.repository.SaleRepository;
 import jp.co.sss.project.repository.TaxRepository;
@@ -42,6 +47,11 @@ public class IndexController {
 	TaxRepository taxrepository;
 	@Autowired
 	SaleRepository salerepository;
+	@Autowired
+	HistRepository histrepository;
+	@Autowired
+	HistdetailRepository histdetailrepository;
+	
 	
 	/*トップページ（ログイン画面の表示）*/
 	@RequestMapping("/index")
@@ -63,9 +73,10 @@ public class IndexController {
 				session.setAttribute("employeeId", form.getEmployeeId());
 				session.setAttribute("employeeNameSei", employeerepository.findEmployeeNameSei(Integer.valueOf(form.getEmployeeId())));
 				session.setAttribute("employeeNameMei", employeerepository.findEmployeeNameMei(Integer.valueOf(form.getEmployeeId())));
-				model.addAttribute("goodses",goodsrepository.findAll());
+				List<Goods> goodsList = goodsrepository.findAllWithDeleteFlg();
+				model.addAttribute("goodses",goodsList);
 				List<Integer> list = new ArrayList<>();
-				for(int i = 0 ; i < 3 ; i++) {
+				for(int i = 0 ; i < goodsList.size() ; i++) {
 					list.add(0);
 				}
 				model.addAttribute("numbers", list);
@@ -103,29 +114,28 @@ public class IndexController {
 	/*商品入力画面に遷移する*/
 	@RequestMapping("/product_input")
 	public String showProductInput(Model model) {
-		model.addAttribute("goodses",goodsrepository.findAll());
+		List<Goods> goodsList = goodsrepository.findAllWithDeleteFlg();
+		model.addAttribute("goodses",goodsList);
+		model.addAttribute("goodsNumber",goodsList.size());
 		List<Integer> list = new ArrayList<>();
-		for(int i = 0 ; i < 3 ; i++) {
+		for(int i = 0 ; i < goodsList.size() ; i++) {
 			list.add(0);
 		}
 		model.addAttribute("numbers", list);
 		return "product_input";
 	}
-	/*商品入力画面に戻る*/
-	@RequestMapping("/product_input_return")
-	public String showProductInputReturn(Model model, ProductNumberList form, int id) {
-		model.addAttribute("goodses",goodsrepository.findAll());
-		model.addAttribute("numbers", form.getNumber());
-		model.addAttribute("id", id);
-		return "product_input";
-	}
+	
 	/*商品入力画面から確認画面に遷移*/
 	@RequestMapping(path = "/product_input_confirm", params = "conf", method=RequestMethod.POST)
-	public String showProductInputConfirm(Model model, ProductNumberList form, int id) {
-		model.addAttribute("customer", customerrepository.getOne(id));
+	public String showProductInputConfirm(Model model, ProductNumberList form, String id) {
+		if(id.equals("")) {
+			model.addAttribute("customer", customerrepository.getOne(0));
+		} else {
+			model.addAttribute("customer", customerrepository.getOne(Integer.parseInt(id)));
+		}
 		
 		/*商品を全件検索してリストに格納*/
-		List<Goods> goodsList = goodsrepository.findAll();
+		List<Goods> goodsList = goodsrepository.findAllWithDeleteFlg();
 		/*小計を格納するリストを宣言*/
 		List<Integer> subTotalList = new ArrayList<>();
 		for(int i = 0 ; i < goodsList.size() ; i++) {
@@ -147,16 +157,17 @@ public class IndexController {
 		//nullチェックしたい
 		int discount = salerepository.findDiscountBetween();
 		sale = subTotal * discount / 100;
+		model.addAttribute("discount", discount);
 		model.addAttribute("sale", -sale);
 		/*
 		 * 消費税を計算
 		 * 今回は10%なので、切り捨てなど考慮していない
 		 */
-		int tax = 0;
-		Tax t = new Tax();
-		t = taxrepository.getOne(1);
-		tax = subTotal * t.getTax() / 100;
+		int salesTax = 0;
+		int tax = taxrepository.getOne(1).getTax();
+		salesTax = subTotal * tax / 100;
 		model.addAttribute("tax", tax);
+		model.addAttribute("salesTax", salesTax);
 		/*
 		 * 最終的な合計を計算
 		 */
@@ -166,10 +177,45 @@ public class IndexController {
 		
 		return "product_input_con";
 	}
+	/*商品入力画面に戻る*/
+	@RequestMapping(path = "/product_input_return", method = RequestMethod.POST)
+	public String showProductInputReturn(Model model, ProductNumberList form, int id) {
+		model.addAttribute("goodses",goodsrepository.findAllWithDeleteFlg());
+		model.addAttribute("numbers", form.getNumber());
+		model.addAttribute("id", id);
+		return "product_input";
+	}
 	/*確認画面から確定画面への遷移
-	 * 履歴テーブルと履歴詳細テーブルｎ*/
-	@RequestMapping("/product_success")
-	String showProductSuccess() {
+	 * データの受け渡し*/
+	@RequestMapping(path = "/product_success", method = RequestMethod.GET)
+	String showProductSuccess(Model model, ProductNumberList form, int id, int discount, int tax) {
+		//histテーブル登録
+		Hist hist = new Hist();
+		Customer customer = new Customer();
+		customer.setId(id);
+		hist.setCustomer(customer);
+		hist.setBuyDate(new Date());
+		hist.setDiscount(discount);
+		hist.setTax(tax);
+		Hist h = histrepository.saveAndFlush(hist);
+		
+		
+		//histdetailテーブル登録
+		//商品の数だけ登録をする
+		List<Integer> histdetailIds = new ArrayList<>();
+		List<Goods> goodsList = goodsrepository.findAllWithDeleteFlg();
+		for(int i = 0 ; i < goodsList.size() ; i++) {
+			Histdetail histdetail = new Histdetail();
+			Hist hist1 = new Hist();
+			hist1.setId(hist.getId());
+			histdetail.setHist(hist1);
+			histdetail.setGoods(goodsList.get(i));
+			histdetail.setCount(form.getNumber().get(i));
+			histdetail.setPrice(goodsList.get(i).getPrice());
+			Histdetail hd = histdetailrepository.saveAndFlush(histdetail);
+			histdetailIds.add(histdetail.getId());
+		}
+		model.addAttribute("histdetailIds", histdetailIds);
 		return "product_success";
 	}
 	/*商品入力画面から顧客選択するためのダイアログ表示*/
@@ -177,6 +223,33 @@ public class IndexController {
 	String searchCustomer() {
 		return "customer_dia";
 	}
-	
+	/*商品履歴一覧画面への遷移*/
+	@RequestMapping("/product_hist")
+	public String showAllProductHist(Model model) {
+		//履歴をリクエストスコープに保存
+		List<Hist> hists = histrepository.findAll();
+		model.addAttribute("hists", hists);
+		
+		//合計数のリストをリクエストスコープに保存
+		List<String> totalList = new ArrayList<>();
+		for(int i = 1 ; i <= hists.size() ; i++) {
+			//hist_idごとの合計数をDBからもってくる
+			Hist h = new Hist();
+			h.setId(i);
+			String total = histdetailrepository.findTotalGroupByHistId(h);
+			//nullが返ってきたら、0をリストに入れる
+			if(total == null) { total = "0"; }
+			totalList.add(total);
+		}
+		model.addAttribute("totalList", totalList);
+		
+		return "product_hist";
+	}
+	/*商品履歴詳細への遷移*/
+	@RequestMapping("/product_hist_detail/{id}")
+	public String showProductHistDetail(Model model, @PathVariable int id) {
+		model.addAttribute("histdetails", histdetailrepository.getOne(id));
+		return "product_hist_detail";
+	}
 	
 }
